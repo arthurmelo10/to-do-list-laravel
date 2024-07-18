360f3cabd9c3f67dea75e79dfea672738e50b63e
arthurmelo10 <arthurmelo1000@gmail.com>
2024-07-18 19:42:45 -0300

    feat: add Todo class in the composer json

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Routing\Controller as BaseController;
 use ToDo\Repository\Repository;
 
-class Controller
+class Controller extends BaseController
 {
     public function __construct(private readonly Repository $repository)
     {
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\Controller as Controller;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
 //    return view('welcomeDraft');
 //});
 
-Route::get('/', [Controller::class, 'getFirstToDo']);
+Route::get('/', [Controller::class, 'getFirstToDo'])->name('getFirstToDo');
